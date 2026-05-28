e32b5f8a2377be24b6c0b1e800568d6067192f21
Romain Authier <romainauthier@outlook.com>
2026-05-28 22:24:30 +0200

    Route add dog

--- routes/dogs_sql.js
+++ routes/dogs_sql.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const Pool = require("../db");
+const { body, validationResult } = require("express-validator");
+
+const validate = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({
+			result: false,
+			message: errors.array()[0].msg,
+		});
+	}
+	next();
+};
 
 // GET /dogs - Récupérer tous les chiens
 router.get("/", async (req, res) => {
@@ -19,33 +31,47 @@ router.get("/", async (req, res) => {
 });
 
 // POST /dogs - Ajouter un nouveau chien
-router.post("/", async (req, res) => {
-	const { name, human, birth_date, gender, race1, race2 } = req.body;
+router.post(
+	"/",
+	[
+		body("name")
+			.notEmpty()
+			.withMessage("Cette boule de poil a forcément un nom !"),
+		body("human").notEmpty().withMessage("Impossible d'itentifier l'humain.e"),
+	],
+	validate,
+	async (req, res) => {
+		const { name, human, birth_date, gender, race1, race2 } = req.body;
 
-	try {
-		const checkDog = await Pool.query(
-			"SELECT * FROM dogs WHERE human = $1 AND name = $2",
-			[human, name],
-		);
+		try {
+			const checkDog = await Pool.query(
+				"SELECT * FROM dogs WHERE human = $1 AND name = $2",
+				[human, name],
+			);
 
-		if (checkDog.rowsCount > 0) {
-			return res.status(400).json({
-				result: false,
-				message: "Un animal à ce nom existe déjà pour cet.te humain.e",
+			if (checkDog.rowsCount > 0) {
+				return res.status(400).json({
+					result: false,
+					message: "Un animal à ce nom existe déjà pour cet.te humain.e",
+				});
+			}
+
+			const sqlResult = await Pool.query(
+				"INSERT INTO dogs (name, human, birth_date, gender, race1, race2) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+				[name, human, birth_date, gender, race1, race2],
+			);
+
+			res.status(200).json({
+				result: true,
+				savedDog: sqlResult.rows[0],
+				message: `${sqlResult.rows[0].name} a été ajouté !`,
 			});
+		} catch (err) {
+			console.error(err);
+			res.status(500).json({ result: false, message: err.message });
 		}
-
-		const sqlResult = await Pool.query(
-			"INSERT INTO dogs (name, human, birth_date, gender, race1, race2) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-			[name, human, birth_date, gender, race1, race2],
-		);
-
-		res.status(200).json({ result: true, savedDog: sqlResult.rows[0] });
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ result: false, message: err.message });
-	}
-});
+	},
+);
 
 // DELETE Supprimer un chien
 router.delete("/:dogId", async (req, res) => {
