a211cf3b2ac3fdf88c41081c8bd676c9d28488bf
Romain Authier <romainauthier@outlook.com>
2026-06-03 19:21:43 +0200

    Config DB cloud Neon

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
 const pg = require("pg");
 
-const Pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-});
+let Pool;
+
+if (process.env.DB_CONNECTION_STRING) {
+	Pool = new pg.Pool({
+		connectionString: process.env.DB_CONNECTION_STRING,
+		ssl: { rejectUnauthorized: false },
+	});
+} else {
+	Pool = new pg.Pool({
+		user: process.env.DB_USER,
+		host: process.env.DB_HOST,
+		database: process.env.DB_NAME,
+		password: process.env.DB_PASSWORD,
+		port: process.env.DB_PORT,
+	});
+}
 
 module.exports = Pool;
